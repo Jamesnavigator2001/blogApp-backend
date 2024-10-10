bfb95d21b0b908263c3f219fb7357440e7179a84
Jamesnavigator2001 <jamesnavigator04@gmail.com>
2024-10-10 17:22:43 +0300

    Search engine changes

--- Blog/urls.py
+++ Blog/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('api/post/<str:post_id>/comment/', PostCommentView.as_view(), name='post_comment'),
     path('api/posts/<str:author_email>/', AuthorPostsView.as_view(), name='author_posts'),
     path('api/comments/<str:post_id>/', CommentListView.as_view(), name='comment_list'),
-    re_path(r'^api/post/(?P<post_id>\d+)/view/(?P<registration_number>[\w\/\-]+)/$', RecordPostView.as_view(), name='record-post-view'),
+    re_path(r'^api/post/(?P<post_id>[\w\-]+)/view/(?P<registration_number>[\w\/\-]+)/$', RecordPostView.as_view(), name='record-post-view'),
 
     # Delete posts
     path('api/posts/delete-all/', DeleteAllPosts.as_view(), name='delete_all_posts'),
